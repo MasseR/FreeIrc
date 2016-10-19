5d686a190332a9d32a86c74fcbce40cfcff57489
Mats Rauhala <mats.rauhala@iki.fi>
2016-10-19 23:18:30 +0300

    Title hook

---
+++ src/Hooks/Title.hs
@@ -0,0 +1,44 @@
+{-# Language OverloadedStrings #-}
+module Hooks.Title where
+
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import Hooks.Algebra
+import Network.IRC
+import Data.ByteString.Lazy (ByteString)
+import qualified Data.ByteString.Lazy as LBS
+import Text.StringLike (castString)
+import Text.HTML.TagSoup
+import Data.Maybe (listToMaybe)
+import Data.Char (isSpace)
+
+urlTitleHook :: InMsg -> Irc ()
+urlTitleHook (PrivMsg _nick target msg) =
+  case () of
+       () | "http://" `T.isInfixOf` msg ->
+         maybe (return ()) (sendMessage . Msg target) =<< (handleWeb . parseUrl "http://" $ msg)
+       () | "https://" `T.isInfixOf` msg ->
+         maybe (return ()) (sendMessage . Msg target) =<< (handleWeb . parseUrl "https://" $ msg)
+       _ -> return ()
+urlTitleHook _ = return ()
+
+parseTitle :: ByteString -> Maybe Text
+parseTitle body = let
+  tags = parseTags body :: [Tag ByteString]
+  titleStr = "title" :: ByteString
+  titleLst = takeWhile (~/= TagClose titleStr) . dropWhile (~== TagOpen titleStr []) $ tags
+  title = T.strip . castString . innerText $ titleLst
+  in if T.null title then Nothing else Just title
+
+
+handleWeb :: Text -> Irc (Maybe Text)
+handleWeb url = do
+  Payload _ body <- fetch (T.unpack url)
+  return $ parseTitle body
+
+parseUrl :: Text -> Text -> Text
+parseUrl splitter msg = let
+  (_, start) = T.breakOn splitter msg
+  url = T.takeWhile (not . isSpace) start
+  in url

43611cbd81c71796f1a6822568200ffddb200720
Mats Rauhala <mats.rauhala@iki.fi>
2016-11-12 22:37:10 +0200

    Renaming

--- src/Chat.hs
+++ src/Chat.hs
@@ -21,29 +21,27 @@ default (Text)
 data ActionF a = Respond Text a deriving (Functor)
 data VoidF a
 
-type Action f a = Free (Sum ActionF f) a
-type SAction f a = SentenceState -> Action f a
-type RAction f a = ReaderT SentenceState (Free (Sum ActionF f)) a
+type Action f a = ReaderT SentenceState (Free (Sum ActionF f)) a
 type SentenceState = Map Text [Text]
 
 
-respond :: Functor f => Text -> RAction f ()
+respond :: Functor f => Text -> Action f ()
 respond r = liftF (InL (Respond r ()))
 
 
 -- inspired by aiml
-newtype Matchable = Matchable [Text]
-newtype MatchState a = MatchState {unMatchState :: State (Map Text [Text]) a}
+newtype Normalized = Normalized [Text]
+newtype SentenceStateB a = SentenceStateB {unSentenceStateB :: State (Map Text [Text]) a}
     deriving (Functor, Applicative, Monad, MonadState (Map Text [Text]))
-data MatchWord = WordMatch !Text
-               | Star
-               | NamedStar !Text
-               deriving (Show, Eq, Ord)
+data Matcher = WordMatch !Text
+             | Star
+             | NamedStar !Text
+             deriving (Show, Eq, Ord)
 
 data MatchTree f = Root [MatchTree f]
-                 | Leaf !MatchWord [MatchTree f] (Maybe (RAction f ()))
+                 | Leaf !Matcher [MatchTree f] (Maybe (Action f ()))
 
-match :: MatchWord -> Text -> MatchState Bool
+match :: Matcher -> Text -> SentenceStateB Bool
 match (WordMatch x) w | x == w = return True
                       | otherwise = return False
 match Star w = modify (M.insertWith (<>) "*" [w]) >> return True
@@ -69,10 +67,10 @@ anyM f = go
         go [] = return False
         go (x:xs) = f x >>= \x' -> if x' then return True else go xs
 
-matchTree :: Matchable -> MatchTree f -> MatchState [RAction f ()]
-matchTree (Matchable xs) = go xs
+matchTree :: Normalized -> MatchTree f -> SentenceStateB [Action f ()]
+matchTree (Normalized xs) = go xs
     where
-        go :: [Text] -> MatchTree f -> MatchState [RAction f ()]
+        go :: [Text] -> MatchTree f -> SentenceStateB [Action f ()]
         go [] (Leaf _ _ f) = return (maybeToList f)
         go ws (Root cs) = concat <$> mapM (go ws) cs
         go [w] (Leaf m _ f) = match m w >>=
@@ -81,14 +79,14 @@ matchTree (Matchable xs) = go xs
             \matched -> if matched then concat <$> mapM (go ws) cs else return []
 
 
-build :: Functor f => [(RAction f (), Text)] -> MatchTree f
+build :: Functor f => [(Action f (), Text)] -> MatchTree f
 build ws =
     case foldr (flip add . uncurry fromText) [] ws of
          [Root cs] -> Root cs
          _ -> Root []
 
 
-fromText :: RAction f () -> Text -> MatchTree f
+fromText :: Action f () -> Text -> MatchTree f
 fromText f = Root . addAction . go . T.words . T.toUpper
     where
         go ("*":ws) = [Leaf Star (go ws) Nothing]
@@ -100,10 +98,10 @@ fromText f = Root . addAction . go . T.words . T.toUpper
         addAction [Leaf w cs _] = [Leaf w (addAction cs) Nothing]
         addAction x = x
 
-evaluateMatcher :: Functor f => Text -> MatchTree f -> Action f ()
+evaluateMatcher :: Functor f => Text -> MatchTree f -> Free (Sum ActionF f) ()
 evaluateMatcher sentence tree =
-    let (f,s) = runState (unMatchState (matchTree normalizedSentence tree)) M.empty
-        normalizedSentence = Matchable . T.words . T.toUpper $ sentence
+    let (f,s) = runState (unSentenceStateB (matchTree normalizedSentence tree)) M.empty
+        normalizedSentence = Normalized . T.words . T.toUpper $ sentence
     in runReaderT (sequence_ f) s
 
 interpretDebug :: Sum ActionF Identity a -> Writer [Text] a

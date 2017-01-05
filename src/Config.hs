5eba79642ecbe87c57bd065d8bbd6b68d2c68b33
Mats Rauhala <mats.rauhala@iki.fi>
2017-01-05 23:39:15 +0200

    Yaml style config

--- src/Config.hs
+++ src/Config.hs
@@ -3,24 +3,6 @@ module Config where
 
 import Network.IRC.Runner
 import Network.IRC
-import Hooks.Algebra
-import Hooks.Title
-import Hooks.Weather
-import qualified Data.Text as T
-
-
-adminHook :: InMsg -> Irc ()
-adminHook (PrivMsg _nick _target msg) =
-  case T.words msg of
-       ["!join", channel] -> sendMessage (Join channel)
-       _ -> return ()
-adminHook _ = return ()
-
-echoHook :: InMsg -> Irc ()
-echoHook (PrivMsg _nick target msg) = sendMessage (Msg target msg)
-echoHook _ = return ()
-
-
 
 defaultMain :: IrcInfo -> IO ()
 defaultMain conf = do
--- src/Main.hs
+++ src/Main.hs
@@ -1,7 +1,55 @@
 {-# Language OverloadedStrings #-}
+{-# Language DeriveGeneric #-}
+{-# Language RecordWildCards #-}
 module Main where
 
 import Config
+import Data.Yaml.Config
+import Data.Yaml
+import Data.Aeson (FromJSON, ToJSON)
+import GHC.Generics (Generic)
+import Data.Text (Text)
+import Network.IRC.Runner (HookBuilder, newHook, IrcInfo(..))
+import Hooks.Algebra
+import Hooks.Title
+import Hooks.Weather
+import Network.IRC
+import qualified Data.Text as T
+
+data ConnectionConf = ConnectionConf { hostname :: Text
+                                     , port :: Int} deriving (Generic, Show)
+data HookConf = HookConf { darkskyApiKey :: String } deriving (Generic, Show)
+data Configuration = Configuration { connection :: ConnectionConf
+                                   , hooksConf :: HookConf
+                                   } deriving (Generic, Show)
+
+instance FromJSON ConnectionConf
+instance FromJSON HookConf
+instance FromJSON Configuration
+
+instance ToJSON ConnectionConf
+instance ToJSON HookConf
+instance ToJSON Configuration
+
+adminHook :: InMsg -> Irc ()
+adminHook (PrivMsg _nick _target msg) =
+  case T.words msg of
+       ["!join", channel] -> sendMessage (Join channel)
+       _ -> return ()
+adminHook _ = return ()
+
+echoHook :: InMsg -> Irc ()
+echoHook (PrivMsg _nick target msg) = sendMessage (Msg target msg)
+echoHook _ = return ()
+
+myHooks :: HookConf -> HookBuilder ()
+myHooks HookConf{..} = do
+    newHook urlTitleHook
+    newHook adminHook
+    newHook $ weatherHook $ ApiKey darkskyApiKey
 
 main :: IO ()
-main = defaultMain defaultConf
+main = do
+    conf <- loadYamlSettings ["config/irc.yaml"] [] ignoreEnv :: IO Configuration
+    defaultMain defaultConf {hooks = myHooks $ hooksConf conf}
+

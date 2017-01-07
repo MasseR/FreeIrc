1c8f5729264a5b23f23b38eae33910c45e139749
Mats Rauhala <mats.rauhala@iki.fi>
2017-01-07 22:35:08 +0200

    AutoFree module

--- src/Hooks/Algebra.hs
+++ src/Hooks/Algebra.hs
@@ -1,15 +1,10 @@
 {-# Language DeriveFunctor #-}
-{-# Language TypeFamilies #-}
 {-# Language GeneralizedNewtypeDeriving #-}
 {-# Language OverloadedStrings #-}
-{-# Language ScopedTypeVariables #-}
-{-# Language FlexibleContexts #-}
-{-# Language FlexibleInstances #-}
 {-# Language DataKinds #-}
 {-# Language TypeOperators #-}
 {-# Language PartialTypeSignatures #-}
 {-# Language PolyKinds #-}
-{-# Language MultiParamTypeClasses #-}
 module Hooks.Algebra where
 
 import GHC.TypeLits
@@ -26,8 +21,6 @@ import qualified Data.Text as T
 import Data.Time (UTCTime)
 import qualified Data.Time as Time
 import Data.Acid.Url
-import Data.Functor.Sum
-import Data.Proxy
 
 data IrcF a =
   SendMessage OutMsg a
@@ -104,41 +97,3 @@ respondTarget nick target = if "#" `T.isPrefixOf` target then target else nick
 respondTo :: Text -> Text -> Text -> Irc ()
 respondTo nick trg msg = sendMessage (Msg (respondTarget nick trg) msg)
 
-class Interpreted (f :: * -> *) where
-    interpret :: MonadIO m => f a -> m a
-
-instance Interpreted f => Interpreted (Free f) where
-    interpret f = foldFree interpret f
-
-instance (Interpreted f, Interpreted g) => Interpreted (Sum f g) where
-    interpret (InL f) = interpret f
-    interpret (InR g) = interpret g
-
-instance Interpreted LogF where
-    interpret (Log x next) = liftIO (putStrLn x) >> return next
-
-instance Interpreted CloudApiF where
-    interpret (PutFile name content next) = liftIO (putStrLn $ "Putting " ++ name) >> return next
-    interpret (GetFile name next) = liftIO (putStrLn $ "Getting " ++ name) >> return (next "asd")
-
-instance Interpreted UrlF where
-    interpret _ = error "Not implemented"
-
-class SumBuilder (x :: * -> *) (f :: [* -> *]) where
-    type Eff f :: * -> *
-    autoHoist :: x a -> Proxy f -> Free (Eff f) a
-
-instance SumBuilder (Free f) ('[f] :: [* -> *]) where
-    type Eff '[f] = f
-    autoHoist f _ = f
-
-instance (Functor f, Functor (Eff (g ': xs))) => SumBuilder (Free f) ((f :: * -> *) ': g ': xs) where
-    type Eff (f ': g ': xs) = Sum f (Eff (g ': xs))
-    autoHoist f _ = hoistFree InL f
-
-instance (Functor f, Functor (Eff (g ': xs)), SumBuilder (Free g) (g ': xs)) => SumBuilder (Free g) (f ': g ': xs) where
-    type Eff (f ': g ': xs) = Sum f (Eff (g ': xs))
-    autoHoist g _ = hoistFree InR (autoHoist g p)
-        where
-            p :: Proxy (g ': xs)
-            p = Proxy

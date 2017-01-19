5a32ce5901370b523525c8548ee336c6dceaf5c4
Mats Rauhala <mats.rauhala@iki.fi>
2017-01-19 21:08:06 +0200

    plusones

--- src/Data/Acid/Database.hs
+++ src/Data/Acid/Database.hs
@@ -25,7 +25,7 @@ import Data.Data
 import Data.Acid (AcidState, Query, Update, makeAcidic, openLocalStateFrom)
 import Data.Acid.Advanced (query', update')
 import Data.Acid.Local (createCheckpointAndClose)
-import Data.SafeCopy (base, deriveSafeCopy)
+import Data.SafeCopy (base, extension, Migrate(..), deriveSafeCopy)
 import Data.Text (Text)
 import Data.Time
 import Data.Map.Strict (Map)
@@ -40,17 +40,27 @@ data UrlRecord = UrlRecord {
   , _time :: !UTCTime
   } deriving (Eq, Ord, Show, Read, Data, Typeable)
 
-data IrcState = IrcState {
-  _urls :: Map Text [UrlRecord]
+data IrcState_0 = IrcState_0 {
+  _urls_0 :: Map Text [UrlRecord]
   } deriving (Eq, Ord, Show, Read, Data, Typeable)
 
+type Nick = Text
+data IrcState = IrcState { _urls :: Map Text [UrlRecord]
+                         , _plusOnes :: Map Nick Int
+                         }
+
+instance Migrate IrcState where
+  type MigrateFrom IrcState = IrcState_0
+  migrate i = IrcState (_urls_0 i) mempty
+
 initialIrcState :: IrcState
-initialIrcState = IrcState mempty
+initialIrcState = IrcState mempty mempty
 
 $(makeLenses ''UrlRecord)
 $(makeLenses ''IrcState)
 $(deriveSafeCopy 0 'base ''UrlRecord)
-$(deriveSafeCopy 0 'base ''IrcState)
+$(deriveSafeCopy 0 'base ''IrcState_0)
+$(deriveSafeCopy 1 'extension ''IrcState)
 
 addUrl :: Text -> UrlRecord -> Update IrcState ()
 addUrl url r = modifying urls (M.insertWith (<>) url [r])

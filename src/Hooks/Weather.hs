835325dcee3af8e8326ffc13a18b43b1c8b3fd67
Mats Rauhala <mats.rauhala@iki.fi>
2017-06-14 11:18:59 +0300

    Restore weather plugin
    Change-Id: Iac89e8eb1d2533bc2f046498e88ead7bb6c1015c

--- src/Hooks/Weather.hs
+++ src/Hooks/Weather.hs
@@ -1,10 +1,13 @@
 {-# Language TemplateHaskell #-}
 {-# Language RecordWildCards #-}
 {-# Language OverloadedStrings #-}
+{-# Language FlexibleContexts #-}
 module Hooks.Weather where
 
 import Control.Lens
 import Control.Monad.Trans.Maybe
+import Control.Monad.Trans (liftIO, lift)
+import Control.Monad.Reader (asks)
 import Data.Monoid ((<>))
 import Data.Text (Text)
 import qualified Data.Text as T
@@ -14,49 +17,45 @@ import Hooks.Algebra
 import Network.Wreq
 import Network.IRC
 import Text.Printf
+import Network.Wreq
+import Types
 
 data Coord = Coord {_latitude :: Double, _longitude :: Double} deriving Show
 data Weather = Weather { _temperature :: Double, _feelsLike :: Double, _icon :: Text } deriving Show
 
-nominatim :: Text -> Irc (Maybe Coord)
 nominatim city = do
     let url = "http://nominatim.openstreetmap.org/search"
         params = defaults & param "format" .~ ["json"]
                           & param "city" .~ [city]
-    Payload _ bs <- fetchWith params url
-    let json = decode bs :: Maybe Value
-        coords = Coord <$> datapoint json "lat" <*> datapoint json "lon"
+    r <- liftIO $ getWith params url
+    let coords = Coord <$> datapoint r "lat" <*> datapoint r "lon"
     return coords
     where
-        datapoint json k = read . T.unpack <$> json ^? _Just . values . key k . _String
+        datapoint json k = read . T.unpack <$> json ^? responseBody . key k . _String
 
-fetchWeather :: ApiKey -> Coord -> Irc (Maybe Weather)
 fetchWeather (ApiKey apiKey) Coord{..} = do
     let url = printf "https://api.darksky.net/forecast/%s/%f,%f" apiKey _latitude _longitude :: String
-    Payload _ bs <- fetch url
-    let weather = Weather <$> fmap fahrenheitToCelcius (datapoint json "temperature")
-                          <*> fmap fahrenheitToCelcius (datapoint json "apparentTemperature")
-                          <*> json ^? _Just . key "currently" . key "icon" . _String
-        json = decode bs :: Maybe Value
+    r <- liftIO $ get url
+    let weather = Weather <$> fmap fahrenheitToCelcius (datapoint r "temperature")
+                          <*> fmap fahrenheitToCelcius (datapoint r "apparentTemperature")
+                          <*> r ^? responseBody . key "currently" . key "icon" . _String
     return weather
     where
-        datapoint json k = json ^? _Just . key "currently" . key k . _Number
+        datapoint json k = json ^? responseBody . key "currently" . key k . _Number
         fahrenheitToCelcius f = fromRational $ toRational (f - 32) / 1.8
 
 newtype ApiKey = ApiKey String deriving Show
 
-weather :: ApiKey -> Text -> Irc (Maybe Weather)
-weather apiKey city = runMaybeT $ do
+weather city = runMaybeT $ do
+    apiKey <-lift $ asks readStateApp
     coords <- MaybeT $ nominatim city
     MaybeT $ fetchWeather apiKey coords
 
-formatWeather :: Weather -> Text
 formatWeather = T.pack . show
 
-weatherHook :: ApiKey -> InMsg -> Irc ()
-weatherHook apiKey (PrivMsg nick target msg) =
+weatherHook (PrivMsg nick target msg) =
     case T.words msg of
-         ["!weather", city] -> weather apiKey city >>= respond
+         ["!weather", city] -> weather city >>= respond
          _ -> return ()
     where
         respond Nothing = return ()

ff4ca7941b606b337c17a129600e7bd187bfaaab
Mats Rauhala <mats.rauhala@iki.fi>
2018-01-19 00:07:48 +0200

    Djinn hook
    Change-Id: Iaacb8db225a91ad05000310a2d405c579264b755

--- src/Hooks/Eval.hs
+++ src/Hooks/Eval.hs
@@ -6,11 +6,13 @@ module Hooks.Eval where
 import ClassyPrelude
 import Data.Conduit.Process
 import Data.Conduit
+import qualified Data.Conduit.Combinators as C
 import qualified Data.Conduit.List as CL
 import qualified Data.Conduit.Text as Text
 import Network.IRC
 import Hooks.Algebra
 import Types
+import Control.Monad.Logger
 
 type Expression = String
 
@@ -20,10 +22,20 @@ mueval expr = snd <$> sourceProcessWithConsumer p consumer
         p = proc "mueval" ["--expression", expr]
         consumer = Text.decodeUtf8 .| Text.lines .| CL.consume
 
+-- XXX: Do a monadlogger instance with mmorph and monadcontrol
+djinn :: (MonadThrow m, MonadIO m) => Text -> m [Text]
+djinn expr = snd' <$> liftIO (sourceProcessWithStreams p (yield $ encodeUtf8 expr) consumer C.stderr)
+    where
+        snd' (_,x,_) = x
+        p = proc "djinn" ["/dev/stdin"]
+        consumer = Text.decodeUtf8 .| Text.lines .| CL.consume
+
+
 
 evalHook (PrivMsg nick target msg) =
     case words msg of
          (">" : rest) -> mueval (unpack $ unwords rest) >>= respond
+         (">>" : rest) -> djinn ("? " <> unwords rest) >>= respond
     where
         respond [] = return ()
         respond xs = mapM_ (respondTo nick target . take 1024) . take 3 $ xs
